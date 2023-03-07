99f569019c79c11553306c7e204fb20308804bdd
Zobayer-Mahmud99 <zobayer.mahmud@parallaxlogic.net>
2023-03-07 16:37:19 +0600

    provider remove

--- lib/view/home_screen.dart
+++ lib/view/home_screen.dart
@@ -1,21 +1,63 @@
 import 'package:flutter/material.dart';
+import 'package:sqflite_practice/controller/home_provider.dart';
+import 'package:sqflite_practice/helper/sqfhelper.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
 
   @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  
+     List<Map<String, dynamic>> journal = [];
+  bool isLoading = false;
+
+  void refreshJournals() async {
+    isLoading = true;
+    
+    final data = await SqlHelper.getItems();
+    journal = data;
+    isLoading = false;
+    print(">>> Number of items in list ${journal.length}");
+    
+  }
+
+  @override void initState() {
+refreshJournals();
+    super.initState();
+   refreshJournals();
+  }
+  @override
   Widget build(BuildContext context) {
+
     return Scaffold(
       appBar: AppBar(
         title: const Text("Sqlite Practice"),
         centerTitle: true,
         backgroundColor: Colors.purple,
       ),
-      body: Container(),
+      body: RefreshIndicator(
+          displacement: 250,
+          backgroundColor: Colors.yellow,
+          color: Colors.red,
+          strokeWidth: 3,
+          triggerMode: RefreshIndicatorTriggerMode.onEdge,
+          onRefresh: () async {
+            await Future.delayed(const Duration(milliseconds: 1500));
+
+       refreshJournals();
+          },
+          child: Container(
+            height:MediaQuery.of(context).size.height,
+            color: Colors.yellow[200],
+          )),
       floatingActionButton: FloatingActionButton(
-          onPressed: () {},
-          backgroundColor: Colors.purple,
-          child: const Icon(Icons. add),),
+        onPressed: () {},
+        backgroundColor: Colors.purple,
+        child: const Icon(Icons.add),
+      ),
     );
   }
 }
